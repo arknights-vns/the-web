34dbfca75477f96052dba3d5d2e9ca1fbae869e8
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-06-29 21:10:02 +0700

    feat(currPlayer): actually send back the player name

--- app/tournament/api/controller/currPlayer/route.ts
+++ app/tournament/api/controller/currPlayer/route.ts
@@ -1,16 +1,17 @@
 //Vote control endpoint
-
 /*
     /api/controller/currPlayer
     POST Params: set (String)
     Response: { name: "salt" },
  */
+import { NextRequest } from "next/server";
 
 export async function GET() {
     return Response.json({ name: "salt" }, { status: 200 });
 }
 
-export async function POST(req) {
-    const pName = req.params.name;
-    return Response.json({ name: pName }, { status: 200 });
+export async function POST(req: NextRequest) {
+    const r = await req.text();
+    const data = JSON.parse(r);
+    return Response.json({ name: data.name }, { status: 200 });
 }
