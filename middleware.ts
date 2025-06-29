f9c5df5f1a8fa21826dcd6d73504967824aef33c
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-06-29 21:09:20 +0700

    chore(middleware): implement URL checking

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,3 @@
-import { headers } from "next/headers";
 import type { NextRequest } from "next/server";
 
 export const config = {
@@ -6,8 +5,9 @@ export const config = {
 };
 
 export async function middleware(req: NextRequest) {
-    //todo: run this section only when /tournament/api/controller/*
-    const headerList = await headers();
-    const token = headerList.get("X-VNS-TUSDATHE");
-    if (token !== process.env.TOKEN) return Response.json({ msg: "auth failed" }, { status: 403 });
+    //note: before merging, change url to https://dreamchasers.akvns.org
+    if (req.url.startsWith("http://localhost:3000/tournament/api/controller/")) {
+        const token = req.headers.get("X-VNS-TUSDATHE");
+        if (token !== process.env.TOKEN) return Response.json({ msg: "auth failed" }, { status: 403 });
+    }
 }
